750606bd9b0d9dc65ad5e6b74dd1d3dbeb4c046a
ductan2 <ndt11102003@gmail.com>
2023-05-25 12:35:41 +0700

    render item

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 const system = require('./config/system');
+const itemModal = require('./schemas/items');
+
+// view engine setup
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.set('layout','backend')
+app.set('layout', 'backend');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,25 +25,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.locals.prefixAdmin=system.prefixAdmin;
+app.locals.prefixAdmin = system.prefixAdmin;
+
+mongoose.connect(
+    'mongodb+srv://ductan123:vanlongan123@cluster0.hdfborf.mongodb.net/item_managements'
+);
+
+var db = mongoose.connection;
+db.on('error', () => {
+    console.log('erorr');
+});
+db.once('open', () => {
+    console.log('success');
+});
 
 app.use('/', indexRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,55 @@
 const express = require('express');
 const router = express.Router();
-
+const itemModal = require('../schemas/items');
 router.get('/', (req, res, next) => {
-    res.render('pages/items/list', { title: 'Page - list' });
+    let statusFilter = [
+        { name: 'ALL', value: 'all', count: 0, link: '#', class: 'default' },
+        {
+            name: 'ACTIVE',
+            value: 'active',
+            count: 0,
+            link: '#',
+            class: 'success'
+        },
+        {
+            name: 'INACTIVE',
+            value: 'inactive',
+            count: 0,
+            link: '#',
+            class: 'default'
+        }
+    ];
+     statusFilter.forEach((st, index) => {
+        let condition = {};
+        if (st.value != 'all') {
+            condition = { status: st.name };
+            console.log('condition', condition);
+        }
+      
+        itemModal
+            .countDocuments(condition)
+            .then((data) => {
+              
+                statusFilter[index].count = data;
+            })
+            .catch((err) => console.log(err));
+    });
+    itemModal
+        .find({})
+        .then((items) => {
+            res.render('pages/items/list', {
+                item: items,
+                statusFilter,
+                title: 'Page - list'
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.render('pages/items/list', { title: 'Page - list' });
+        });
 });
 router.get('/add', (req, res, next) => {
     res.render('pages/items/add', { title: 'Page - add' });
 });
-
 
 module.exports = router;
